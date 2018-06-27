916ba6c8f1263ad080ea65060123dcc2527c8acb
Jelle De Loecker <jelle@develry.be>
2018-06-28 00:16:27 +0200

    Make module variables available in the repl, refs #10

--- bin/execbin.js
+++ bin/execbin.js
@@ -3,6 +3,7 @@ var isBinaryFile = require('isbinaryfile'),
     Janeway      = require('../lib/init.js'),
     libpath      = require('path'),
     fs           = require('fs'),
+    vm           = require('vm'),
     main_file,
     buffer,
     line;
@@ -18,7 +19,10 @@ process.argv.splice(1, 1);
 // Start initializing janeway
 Janeway.start(function started(err) {
 
-	var title;
+	var vm_options,
+	    title,
+	    code,
+	    nr;
 
 	if (err) {
 		console.error('Could not start Janeway: ' + err);
@@ -42,13 +46,72 @@ Janeway.start(function started(err) {
 	try {
 		if (main_file) {
 			title = JSON.stringify(process.argv[1]);
-			Janeway.print('info', ['Requiring main file', title]);
 			Janeway.setTitle('Janeway: ' + title);
 
 			if (require.main) {
 				require.main.janeway_required = main_file;
 			}
 
+			if (Janeway.config.execbin.evaluate_files) {
+
+				Janeway.print('info', ['Evaluating main file', title]);
+
+				if (!main_file.endsWith('.js')) {
+					main_file += '.js';
+				}
+
+				code = fs.readFileSync(main_file, 'utf8');
+
+				code = 'var __dirname = ' + JSON.stringify(libpath.dirname(main_file)) + ';'
+				     + 'var __filename = ' + JSON.stringify(libpath.basename(main_file)) + ';'
+				     + code;
+
+				vm_options = {
+					filename    : main_file
+				};
+
+				try {
+					vm.runInContext(code, Janeway.vm_context, vm_options);
+					return;
+				} catch (err) {
+					if (err.message == 'Illegal return statement') {
+						// Try again without the return!
+						nr = err.stack.split('\n')[0].split(':')[1];
+
+						// Split the code
+						code = code.split('\n');
+
+						// Keep only the lines before the return
+						code = code.slice(0, nr);
+
+						// Remove everything from the return
+						code[code.length - 1] = __Protoblast.Bound.String.before(code[code.length - 1], 'return');
+
+						// Join the code again
+						code = code.join('\n');
+
+						try {
+							vm.runInContext(code, Janeway.vm_context, vm_options);
+							return;
+						} catch (err) {
+							if (err.name != 'SyntaxError') {
+								console.error(err);
+								return;
+							}
+						}
+
+					} else {
+						if (err.name != 'SyntaxError') {
+							console.error(err);
+							return;
+						}
+					}
+
+					Janeway.print('info', ['SyntaxError', err, 'evaluating file, falling back to `require`']);
+				}
+			}
+
+			Janeway.print('info', ['Requiring main file', title]);
 			require(main_file);
 		} else {
 			Janeway.setTitle('Janeway');
